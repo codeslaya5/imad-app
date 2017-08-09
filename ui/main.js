8d358539bb39dcf668f6f1303341a000f25da8d2
Murali Vijay Krishnan <vjrockz111@gmail.com>
2017-08-09 19:15:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 
 var element = document.getElementById('main');
 element.innerHTML = 'Hey there!';
+
+//Move the image
+var img = document.getElementById('dino');
+img.onclick = function(){
+    img.style.marginLeft = '100px';
+};
