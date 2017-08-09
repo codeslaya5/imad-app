d1c657df7483fa2d91dddfc7ef45eb9477cd4319
Murali Vijay Krishnan <vjrockz111@gmail.com>
2017-08-09 19:24:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,9 +7,9 @@ element.innerHTML = 'Hey there!';
 var img = document.getElementById('dino');
 var marginLeft = 0;
 function moveRight(){
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 5;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function(){
-    var interval = setInterval(moveRight, 100);
+    var interval = setInterval(moveRight, 50);
 };
