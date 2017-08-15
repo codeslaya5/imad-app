490b9639973bb93c6696fdbfffbd6a538a010650
Murali Vijay Krishnan <vjrockz111@gmail.com>
2017-08-15 20:20:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,6 +75,15 @@ app.get('/counter', function(req,res){
     res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name', function(req,res){
+    //Get the name from the request 
+    var name = req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
+
+
 app.get('/:articleName', function(req, res){
     var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
@@ -85,13 +94,6 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-var names = [];
-app.get('/submit-name', function(req,res){
-    //Get the name from the request 
-    var name = req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-});
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
