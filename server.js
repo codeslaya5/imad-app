0985d429ea06c82f93c7c20481828b20de13acf5
Murali Vijay Krishnan <vjrockz111@gmail.com>
2017-08-15 20:19:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,9 +86,9 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name', function(req,res){
+app.get('/submit-name', function(req,res){
     //Get the name from the request 
-    var name = req.params.name;
+    var name = req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
