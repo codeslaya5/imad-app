6fe04f9d4e98fe32dd1eb1423134cba507d335c7
Murali Vijay Krishnan <vjrockz111@gmail.com>
2017-08-15 20:08:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,3 +40,38 @@ button.onclick = function(){
     request.open('GET','http://vjrockz111.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+//Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+  //Make a request to the server and send the name
+  var names = ['name1' , 'name2' , 'name3'];
+  var list = '';
+  for(var i=0; i<names.length; i++)
+  {
+      list+='<li>' + names[i] + '</li>';
+  }
+  
+  var ul = document.getElementById('');
+  ul.innerHTML = list;
+  //Capture a list of names and render it as a list
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
